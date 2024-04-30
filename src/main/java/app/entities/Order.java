ab244a9b849fe8b678f7a7b3a677971c54349d61
Marcus-Ekeroth <cph-me313@cphbusiness.dk>
2024-04-30 13:59:41 +0200

    getters, setters, tostring in Order

--- src/main/java/app/entities/Order.java
+++ src/main/java/app/entities/Order.java
@@ -18,4 +18,73 @@ public class Order {
         this.status = status;
         this.shippingAddress = shippingAddress;
     }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getRoof() {
+        return roof;
+    }
+
+    public void setRoof(String roof) {
+        this.roof = roof;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", price=" + price +
+                ", width=" + width +
+                ", length=" + length +
+                ", roof='" + roof + '\'' +
+                ", status=" + status +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                '}';
+    }
 }
